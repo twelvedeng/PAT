a6c6ee5a8b9106f2058c8c0da6b405336d1efe3f
twelveD <39939365+twelvedeng@users.noreply.github.com>
2018-11-24 23:30:04 +0800

    Update 1008. 数组元素循环右移问题 (20).cpp

--- BasicLevel_C++/1008. 数组元素循环右移问题 (20).cpp
+++ BasicLevel_C++/1008. 数组元素循环右移问题 (20).cpp
@@ -1,18 +1,14 @@
 #include <iostream>
-#include <algorithm>
 #include <vector>
 using namespace std;
 int main() {
     int n, m;
     cin >> n >> m;
     vector<int> a(n);
-    for (int i = 0; i < n; i++)
-        cin >> a[i];
     m %= n;
-    if (m != 0) {
-        reverse(begin(a), begin(a) + n);
-        reverse(begin(a), begin(a) + m);
-        reverse(begin(a) + m, begin(a) + n);
+    for (int i = 0; i < n; i++, m++) {
+        cin >> a[m];
+        if(m == n-1) m = -1;
     }
     for (int i = 0; i < n - 1; i++)
         cout << a[i] << " ";
